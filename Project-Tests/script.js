3f217a8109304d58fcf2907b5001ffb99858a78e
christian-sarpong <christiananietie10@gmail.com>
2024-10-20 23:40:53 +0000

    added new Javascript statement

--- Project-Tests/script.js
+++ Project-Tests/script.js
@@ -1,2 +1,45 @@
 
-console.log("This is from JS file");
+const resultElement = document.getElementById("result")
+let recognition;
+
+
+
+function startConverting() {
+
+    if('webkitSpeechRecognition' in window)
+        recognition = new webkitSpeechRecognition();
+        setupRecognition(recognition);
+        recognition.start();
+
+}
+
+function setupRecognition(recognition){
+
+    recognition.continuous = true;
+
+    recognition.interimResults = true;
+
+
+    
+
+
+}
+
+
+function processResult(results){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function stopConverting(){
+
+}
